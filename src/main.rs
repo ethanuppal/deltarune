ae5444434d8b1acde83e2d28d2009acc37d7038b
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-07-04 19:49:27 -0700

    feat: Get some sprites on the screen
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@
 use std::{env, time::Duration};
 
 use avian2d::{math::Vector, prelude::*};
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::{
+    image::ImageSamplerDescriptor,
+    prelude::*,
+    window::{PrimaryWindow, WindowResolution},
+};
+use bevy_aseprite_ultra::{prelude::AseSlice, AsepriteUltraPlugin};
 use bevy_ecs_tiled::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 
@@ -40,18 +45,25 @@ fn main() {
     let mut app = App::new();
 
     // Add Bevy default plugins
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "Window title or something".into(),
-            resolution: WindowResolution::new(
-                (RANDOM_SCALE_ON_MY_COMPUTER * DELTARUNE_WIDTH) as f32,
-                (RANDOM_SCALE_ON_MY_COMPUTER * DELTARUNE_HEIGHT) as f32,
-            ),
-            resizable: false,
-            ..default()
-        }),
-        ..default()
-    }))
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Window title or something".into(),
+                    resolution: WindowResolution::new(
+                        (RANDOM_SCALE_ON_MY_COMPUTER * DELTARUNE_WIDTH) as f32,
+                        (RANDOM_SCALE_ON_MY_COMPUTER * DELTARUNE_HEIGHT) as f32,
+                    ),
+                    resizable: false,
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin {
+                default_sampler: ImageSamplerDescriptor::nearest(),
+            }),
+    )
+    .add_plugins(AsepriteUltraPlugin)
     // // Add bevy_ecs_tiled plugin: note that bevy_ecs_tilemap::TilemapPlugin
     // // will be automatically added as well if it's not already done
     // .add_plugins(TiledMapPlugin::default())
@@ -82,13 +94,45 @@ fn main() {
 
 fn startup(
     mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
     mut time: ResMut<Time<Physics>>,
 ) {
     time.pause();
 
+    let window = window_query.get_single().unwrap();
+
+    let width = window.width();
+    let height = window.height();
+
     // Spawn a Bevy 2D camera
     commands.spawn(Camera2d);
+
+    let battle_hud_aseprite = asset_server.load("dr_battle_hud.aseprite");
+
+    commands.spawn((
+        AseSlice {
+            name: "background".into(),
+            aseprite: battle_hud_aseprite.clone(),
+        },
+        Sprite::default(),
+        Transform::from_scale(Vec3::ONE * (RANDOM_SCALE_ON_MY_COMPUTER as f32)),
+    ));
+
+    commands.spawn((
+        AseSlice {
+            name: "tp_red_background".into(),
+            aseprite: battle_hud_aseprite,
+        },
+        Sprite::default(),
+        Transform::from_scale(Vec3::ONE * (RANDOM_SCALE_ON_MY_COMPUTER as f32)).with_translation(
+            Vec3::new(
+                -width / 2.0 + 38.0 * (RANDOM_SCALE_ON_MY_COMPUTER as f32),
+                -height / 2.0 + 244.0 * (RANDOM_SCALE_ON_MY_COMPUTER as f32),
+                0.0,
+            ),
+        ),
+    ));
 
     // // Load a map asset and retrieve the corresponding handle
     // let map_handle: Handle<TiledMap> = asset_server.load("test_level.tmx");
